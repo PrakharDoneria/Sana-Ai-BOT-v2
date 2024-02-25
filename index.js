06f64a8d4fb6c6b787915966a742b000d48e4cd9
user2024git <159011992+user2024git@users.noreply.github.com>
2024-02-25 08:01:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -239,7 +239,8 @@ bot.on("text", async (ctx) => {
 
   } catch (error) {
     console.error('Error during processing:', error.response?.data || error.message);
-    ctx.reply('I ignore!');
+    ctx.reply('Seems their was a small issue while replying please give /new command and wait for 2 minutes');
+    ctx.reply('Error during processing:', error.response?.data || error.message');
   }
 });
 
